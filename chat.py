394f34c177f819c24713f123f455d8a3fea0f70b
Sai Nihal <109018562+Nihalrt@users.noreply.github.com>
2023-07-01 09:56:54 -0700

    Updated Code

--- chat.py
+++ chat.py
@@ -6,33 +6,32 @@ openai.api_key = "Your API Key"
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voices', voices[1].id)
+engine.setProperty('voice', voices[1].id)
 
 r = sr.Recognizer()
 mic = sr.Microphone(device_index=1)
 
-conversation = ""
+conversation = []
 user_name = "Sam"
 bot_name = "Siri"
 
-while True:
+def listen_for_audio():
     with mic as source:
-        print("\n Listening...")
+        print("\nListening...")
         r.adjust_for_ambient_noise(source, duration=0.2)
         audio = r.listen(source)
-    print("no longer listening")
+    print("No longer listening")
 
     try:
         user_input = r.recognize_google(audio)
-    except:
-        continue
+        return user_input
+    except sr.UnknownValueError:
+        return ""
 
-    prompt = user_name+":"+user_input + "\n"+bot_name+":"
-    conversation += prompt
-
+def generate_response(prompt):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=conversation,
+        prompt=prompt,
         temperature=0.7,
         max_tokens=256,
         top_p=1,
@@ -40,12 +39,18 @@ while True:
         presence_penalty=0
     )
 
-    response_str = response["choices"][0]["text"].replace("\n", "")
-    response_str =response_str.split(
-        user_name + ":" ,1)[0].split(bot_name+ ":",1)[0]
+    response_str = response.choices[0].text.strip().replace("\n", "")
+    return response_str
 
-    conversation+= response_str +"\n"
-    print(response_str)
+while True:
+    user_input = listen_for_audio()
+    if user_input:
+        prompt = user_name + ": " + user_input + "\n" + bot_name + ":"
+        conversation.append(prompt)
 
-    engine.say(response_str)
-    engine.runAndWait()
+        response_str = generate_response("\n".join(conversation))
+        conversation.append(response_str + "\n")
+        print(response_str)
+
+        engine.say(response_str)
+        engine.runAndWait()
